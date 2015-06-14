d834209a06b0a0a60b3d23633f61e82861976e3f
msaroufim <marksaroufim@gmail.com>
2015-06-14 01:27:35 +0000

    removed horrible DS files

---
+++ Feature-Extraction/flatten-data.py
@@ -0,0 +1,17 @@
+"""
+A helper script that takes in data of the form
+Input:
+data
+    --1
+        --1.csv: [x_1,y_1] ... [x_n,y_n]
+        ...
+        --200.csv
+    ...
+    --1700
+
+And turns it into the following single csv file
+Output:
+[x_i,y_i,route_j,driver_k]
+
+
+"""
