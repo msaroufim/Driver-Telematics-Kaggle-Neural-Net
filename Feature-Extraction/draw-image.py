a379f25fa2b0f61c9e352c9a8a669a2d930ff54e
Mark Saroufim <marksaroufim@gmail.com>
2015-06-15 22:58:38 -0700

    added multiprocess version of script to read all driver data in parallel. MUCH MUCH FASTER

--- Feature-Extraction/draw-image.py
+++ Feature-Extraction/draw-image.py
@@ -14,6 +14,7 @@ import sys
 from PIL import Image
 import pdb
 import random
+import multiprocessing
 
 def coord_to_graph_image(file_name):
 
@@ -80,56 +81,36 @@ def next_file():
             yield file_name
 
 
-def find_largest_x_y():
-    """
-    Find largest x coordinate and largest y coordinate,
-    Returned values need not be associated to the same point
-
-    This will become obsolete after I implement the flatten operation
-    since I'll only need to do a linear scan
-    """
-    x_max = 0.0
-    y_max = 0.0
-    for driver in os.listdir("drivers"):
-        for route_number in os.listdir(driver):
-            file_name = "drivers/" + driver + route_number
-            with open(file_name) as f:
-                next(f)
-                for line in f:
-                    x,y = line.split(",")
-                    x = max(abs(x), abs(x_max))
-                    y = max(abs(y), abs(y_max))
-
-    print "the maximal values are" %(x,y)
-    return x,y
 
 
-
-def main():
+def main(driver):
     dataset = []
-    data_location = "/Volumes/BIG/drivers/"
-    for driver in os.listdir(data_location):
-        path = data_location + driver
-        for route_number in os.listdir(path):
+    data_location = "/drivers/"
+    #for driver in os.listdir(data_location):
+    path = data_location + driver
+    for route_number in os.listdir(path):
 
-            route_filename = path + '/' + route_number
-            if route_filename.endswith(".png"):
-                continue
-            #image_name = coord_to_graph_image(route_filename)
-            #print "Image name is: " + image_name
+        route_filename = path + '/' + route_number
+        if route_filename.endswith(".png"):
+            continue
+        #image_name = coord_to_graph_image(route_filename)
+        #print "Image name is: " + image_name
 
 
-            #CHANGE IF NEEDED TO DIFFERENT SIZE, TRY TO KEEP IT SQUARE
-            resized_image_name = resize_image_save( coord_to_graph_image(path + '/' + route_number) ,25,25)
-            dataset.append([resized_image_name,driver])
-            #random.shuffle(dataset) will shuffle it properly in place so no return value
+        #CHANGE IF NEEDED TO DIFFERENT SIZE, TRY TO KEEP IT SQUARE
+        resized_image_name = resize_image_save( coord_to_graph_image(path + '/' + route_number) ,25,25)
+        dataset.append([resized_image_name,driver])
+        #random.shuffle(dataset) will shuffle it properly in place so no return value
     print '-'*50
     print dataset
     return dataset
 
 
 if __name__ == "__main__":
-    main()
+    NUM_DRIVERS = 3612
+    for i in range(1,NUM_DRIVERS):
+        p = multiprocessing.Process(target=main,args=(i,))
+        p.start()
     # file_name  = "../1/1.csv"
     # image_name = coord_to_graph_image(file_name)
     # print "created larger image: " + image_name
@@ -153,3 +134,29 @@ if __name__ == "__main__":
     # plt.figure()
     # plt.plot(*zip(*coord_list))
     # plt.show()
+
+
+
+# def find_largest_x_y():
+#     """
+#     Find largest x coordinate and largest y coordinate,
+#     Returned values need not be associated to the same point
+
+#     This will become obsolete after I implement the flatten operation
+#     since I'll only need to do a linear scan
+#     """
+#     x_max = 0.0
+#     y_max = 0.0
+#     for driver in os.listdir("drivers"):
+#         for route_number in os.listdir(driver):
+#             file_name = "drivers/" + driver + route_number
+#             with open(file_name) as f:
+#                 next(f)
+#                 for line in f:
+#                     x,y = line.split(",")
+#                     x = max(abs(x), abs(x_max))
+#                     y = max(abs(y), abs(y_max))
+
+#     print "the maximal values are" %(x,y)
+#     return x,y
+
