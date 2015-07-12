1a6c91b1be7d50d9ba93ef99b1277640773d5c4c
Mark Saroufim <marksaroufim@gmail.com>
2015-07-12 11:03:00 -0700

    dumped newest changes

--- solution/solution.py
+++ solution/solution.py
@@ -24,7 +24,6 @@ import multiprocessing
 
 
 #from __future__ import print_function
-from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
@@ -32,92 +31,92 @@ from keras.utils import np_utils
 import numpy as np
 
 
-
-def coord_to_graph_image(file_name):
-
-
-    G = nx.Graph()
-    with open(file_name) as f:
-        #print "Reading driver_name on route %s" %(file_name)
-        next(f)
-        for i, line in enumerate(f):
-            x,y = line.split(",")
-            print(x,y)
-            G.add_node(i,pos=(x,y))
-
-    pos = nx.get_node_attributes(G,'pos')
-    #print("constructed graph for " , file_name)
-
-    plt.figure()
-    nx.draw(G,pos)
-    #plt.show()
-
-    #print "displaying graph" + file_name
-    #pdb.set_trace()
-    image_name = file_name.replace("csv", "png")
-    plt.savefig(image_name,bbox_inches='tight')
-    #print "saved graph"
-    #print "image name:" + image_name
-    return image_name
-
-
-
-
-
-
-def resize_image_save(infile,width,height):
-    """
-    Given a list of image names, resize them to width x height in pixels
-    ANTIALIAS to preserve quality of the image
-    Save compressed image in the same directory as the larger image
-    """
-    size = width,height
-
-    #pdb.set_trace()
-    #print "resize_image_save---infile: " + infile
-    im = Image.open(infile)
-    im.thumbnail(size,Image.ANTIALIAS)
-    resized_image_name = infile.replace("drivers","thumbnail"+str(height))
-    #resized_image_name = infile.replace(".png", "_small.png")
-
-    if not os.path.exists(os.path.dirname(resized_image_name)):
-        os.makedirs(os.path.dirname(resized_image_name))
-
-
-    im.save(resized_image_name,"png")
-
-    return resized_image_name
-
-
-
-def preprocess(driver):
-    dataset = []
-    data_location = "./drivers/"
-    #for driver in os.listdir(data_location):
-    path = data_location + str(driver)
-    for route_number in os.listdir(path):
-
-        route_filename = path + '/' + route_number
-        if route_filename.endswith(".png"):
-            continue
-        #image_name = coord_to_graph_image(route_filename)
-        #print "Image name is: " + image_name
-
-
-        #CHANGE IF NEEDED TO DIFFERENT SIZE, TRY TO KEEP IT SQUARE
-        resized_image_name = resize_image_save( coord_to_graph_image(path + '/' + route_number) ,28,28)
-
-        resized_image = Image.open(resized_image_name)
-        image_numpy = np.asarray(resized_image)
-
-        dataset.append([image_numpy,driver])
-
-    #random.shuffle(dataset) shuffles dataset in place
-    random.shuffle(dataset)
-    (X_train, y_train) = dataset[0:int(len(dataset)*0.8)][0], dataset[0:int(len(dataset)*0.8)][1]
-    (X_test, y_test) = dataset[int(len(dataset)*0.8 + 1):int(len(dataset))][0], dataset[int(len(dataset)*0.8 + 1):len(dataset)][1]
-    #print '-'*50
-    return dataset
+#
+# def coord_to_graph_image(file_name):
+#
+#
+#     G = nx.Graph()
+#     with open(file_name) as f:
+#         #print "Reading driver_name on route %s" %(file_name)
+#         next(f)
+#         for i, line in enumerate(f):
+#             x,y = line.split(",")
+#             print(x,y)
+#             G.add_node(i,pos=(x,y))
+#
+#     pos = nx.get_node_attributes(G,'pos')
+#     #print("constructed graph for " , file_name)
+#
+#     plt.figure()
+#     nx.draw(G,pos)
+#     #plt.show()
+#
+#     #print "displaying graph" + file_name
+#     #pdb.set_trace()
+#     image_name = file_name.replace("csv", "png")
+#     plt.savefig(image_name,bbox_inches='tight')
+#     #print "saved graph"
+#     #print "image name:" + image_name
+#     return image_name
+#
+#
+#
+#
+#
+#
+# def resize_image_save(infile,width,height):
+#     """
+#     Given a list of image names, resize them to width x height in pixels
+#     ANTIALIAS to preserve quality of the image
+#     Save compressed image in the same directory as the larger image
+#     """
+#     size = width,height
+#
+#     #pdb.set_trace()
+#     #print "resize_image_save---infile: " + infile
+#     im = Image.open(infile)
+#     im.thumbnail(size,Image.ANTIALIAS)
+#     resized_image_name = infile.replace("drivers","thumbnail"+str(height))
+#     #resized_image_name = infile.replace(".png", "_small.png")
+#
+#     if not os.path.exists(os.path.dirname(resized_image_name)):
+#         os.makedirs(os.path.dirname(resized_image_name))
+#
+#
+#     im.save(resized_image_name,"png")
+#
+#     return resized_image_name
+#
+#
+#
+# def preprocess(driver):
+#     dataset = []
+#     data_location = "./drivers/"
+#     #for driver in os.listdir(data_location):
+#     path = data_location + str(driver)
+#     for route_number in os.listdir(path):
+#
+#         route_filename = path + '/' + route_number
+#         if route_filename.endswith(".png"):
+#             continue
+#         #image_name = coord_to_graph_image(route_filename)
+#         #print "Image name is: " + image_name
+#
+#
+#         #CHANGE IF NEEDED TO DIFFERENT SIZE, TRY TO KEEP IT SQUARE
+#         resized_image_name = resize_image_save( coord_to_graph_image(path + '/' + route_number) ,28,28)
+#
+#         resized_image = Image.open(resized_image_name)
+#         image_numpy = np.asarray(resized_image)
+#
+#         dataset.append([image_numpy,driver])
+#
+#     #random.shuffle(dataset) shuffles dataset in place
+#     random.shuffle(dataset)
+#     (X_train, y_train) = dataset[0:int(len(dataset)*0.8)][0], dataset[0:int(len(dataset)*0.8)][1]
+#     (X_test, y_test) = dataset[int(len(dataset)*0.8 + 1):int(len(dataset))][0], dataset[int(len(dataset)*0.8 + 1):len(dataset)][1]
+#     #print '-'*50
+#     return dataset
 
 
 dataset = []
@@ -125,8 +124,8 @@ for driver in os.listdir("./thumbnail28"):
     path = "./thumbnail28/" + str(driver)
     for route_number in os.listdir(path):
         image_numpy = np.asarray(Image.open(path + "/" + route_number))
-        
-        dataset = np.vstack([dataset,[image_numpy,int(driver)]])
+
+        dataset = np.vstack(dataset,[image_numpy,int(driver)])
         #dataset.append([image_numpy,int(driver)])
 
 dataset = np.array(dataset)
